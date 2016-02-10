fff778c8c2b6d4ed3d054f9517d5f12376beb936
Jinghan <phantom.there@gmail.com>
2016-02-10 12:09:26 +0100

    use strict for Drink class

--- models/Drink.js
+++ models/Drink.js
@@ -1,8 +1,9 @@
 /**
  * Created by Jinghan on 8/2/16.
  */
+"use strict"
+
 class Drink {
-    var type, id, name, altName, price, discountPrice, count;
     constructor(type, id, name, altName, price, discountPrice, count) {
         this.type = type;
         this.id = id;
