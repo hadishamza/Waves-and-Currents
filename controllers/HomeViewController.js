0eb0b48b1e5ff8ccb9019dc03e1ba95a3441a284
Jinghan <phantom.there@gmail.com>
2016-02-10 18:50:34 +0100

    refactor HomeViewController

--- controllers/HomeViewController.js
+++ controllers/HomeViewController.js
@@ -2,13 +2,17 @@
  * Created by Jinghan on 8/2/16.
  */
 
-logIn("ervtod", "ervtod", function(success, response) {
-    if (success) {
-        currentUser = response;
-        if (response instanceof Administrator) {
-            console.log("Welcome, Admin");
-        } else {
-            console.log("Welcome, Customer");
+logInAndReigsterCurrentUser("ervtod", "ervtod")
+
+function logInAndReigsterCurrentUser(userName, password) {
+    logIn(userName, password, function(success, response) {
+        if (success) {
+            currentUser = response;
+            if (response instanceof Administrator) {
+                console.log("Welcome, Admin");
+            } else {
+                console.log("Welcome, Customer");
+            }
         }
-    }
-})
+    })
+}
