d3bfa2b2fc760504e555bba0063964ab97f63068
Hadis Hamza <hadis.hamza@gmail.com>
2016-02-18 11:22:09 +0100

    Element localization
    Specifically added for button localization.
    Attention: buttons id and the localized keyword have to match.

---
+++ services/ElementLocalization.js
@@ -0,0 +1,9 @@
+// Localization for elements where we have to get them after creation (for example buttons)
+
+var allButtons = document.querySelectorAll('input[type=button]');
+
+for(i = 0; i < allButtons.length; i++){
+	var id = allButtons[i].id;
+	console.log(id)
+	allButtons[i].setAttribute('value',localizedString(id));
+}
