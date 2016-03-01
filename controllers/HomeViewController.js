9864875284e1774ad067057680d71ae833b22c4c
Jinghan <phantom.there@gmail.com>
2016-03-01 17:16:31 +0100

    bootstrap import, layout for home and menu updated

--- controllers/HomeViewController.js
+++ controllers/HomeViewController.js
@@ -14,3 +14,8 @@ function logInAndRegisterCurrentUser(userName, password) {
         }
     })
 }
+
+$(".nav a").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+});
